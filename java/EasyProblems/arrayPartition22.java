3ffcf62cf1f9b15c16e94ebf924f41c743f86d0a
chandanray917 <chandankumarsunilray@gmail.com>
2026-01-13 23:26:07 +0530

    array partition

---
+++ java/EasyProblems/arrayPartition22.java
@@ -0,0 +1,31 @@
+package EasyProblems;
+
+import java.util.Arrays;
+
+public class arrayPartition22 {
+
+
+    public static int arrayPairSum(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 4, 3, 2};
+        System.out.println("Output: " + arrayPairSum(nums1));
+
+
+        int[] nums2 = {6, 2, 6, 5, 1, 2};
+        System.out.println("Output: " + arrayPairSum(nums2));
+    }
+}
