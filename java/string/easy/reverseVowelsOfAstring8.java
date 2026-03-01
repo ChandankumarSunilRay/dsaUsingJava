a140845dab2f9c5fce6e454faa1e106e04daee30
chandanray917 <chandankumarsunilray@gmail.com>
2026-03-01 11:15:53 +0530

    reverse vowels of a string

---
+++ java/string/easy/reverseVowelsOfAstring8.java
@@ -0,0 +1,51 @@
+package string.easy;
+
+import java.util.Scanner;
+
+public class reverseVowelsOfAstring8 {
+
+    public static String reverseVowels(String s) {
+        char[] arr = s.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            while (left < right && !isVowel(arr[left])) {
+                left++;
+            }
+
+
+            while (left < right && !isVowel(arr[right])) {
+                right--;
+            }
+
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+
+    private static boolean isVowel(char c) {
+        return "aeiouAEIOU".indexOf(c) != -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        String result = reverseVowels(input);
+
+        System.out.println("Output: " + result);
+
+        sc.close();
+    }
+}
