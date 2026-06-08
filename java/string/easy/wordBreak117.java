199d2d516c31868cd68309bff9adacbe235b1bd0
chandanray917 <chandankumarsunilray@gmail.com>
2026-06-08 22:41:31 +0530

    Word Break

---
+++ java/string/easy/wordBreak117.java
@@ -0,0 +1,45 @@
+package string.easy;
+
+import java.util.*;
+
+public class wordBreak117 {
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> dict = new HashSet<>(wordDict);
+
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && dict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        wordBreak117 solution = new wordBreak117();
+
+
+        String s1 = "leetcode";
+        List<String> dict1 = Arrays.asList("leet", "code");
+        System.out.println("Test 1: " + solution.wordBreak(s1, dict1)); // true
+
+
+        String s2 = "applepenapple";
+        List<String> dict2 = Arrays.asList("apple", "pen");
+        System.out.println("Test 2: " + solution.wordBreak(s2, dict2)); // true
+
+
+        String s3 = "catsandog";
+        List<String> dict3 = Arrays.asList("cats", "dog", "sand", "and", "cat");
+        System.out.println("Test 3: " + solution.wordBreak(s3, dict3)); // false
+    }
+}
