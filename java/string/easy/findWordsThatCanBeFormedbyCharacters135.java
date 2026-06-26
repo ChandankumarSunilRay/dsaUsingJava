20f8c1caec7017090b6cc2f34a82cc2bc3fbf797
ChandankumarSunilRay <chandankumarsunilray@gmail.com>
2026-06-27 00:13:24 +0530

    find words that can be formated by characters

---
+++ java/string/easy/findWordsThatCanBeFormedbyCharacters135.java
@@ -0,0 +1,48 @@
+package string.easy;
+public class findWordsThatCanBeFormedbyCharacters135 {
+
+    public int countCharacters(String[] words, String chars) {
+        int[] available = new int[26];
+        for (char ch : chars.toCharArray()) {
+            available[ch - 'a']++;
+        }
+        int totalLength = 0;
+
+        for (String word : words) {
+
+            int[] current = new int[26];
+            boolean canForm = true;
+
+            for (char ch : word.toCharArray()) {
+                current[ch - 'a']++;
+                if (current[ch - 'a'] > available[ch - 'a']) {
+                    canForm = false;
+                    break;
+                }
+            }
+            if (canForm) {
+                totalLength += word.length();
+            }
+        }
+        return totalLength;
+    }
+}
+class Main {
+    public static void main(String[] args) {
+
+        findWordsThatCanBeFormedbyCharacters135 obj =
+                new findWordsThatCanBeFormedbyCharacters135();
+
+        String[] words1 = {"cat", "bt", "hat", "tree"};
+        String chars1 = "atach";
+
+        System.out.println("Example 1 Output: " +
+                obj.countCharacters(words1, chars1));
+
+        String[] words2 = {"hello", "world", "leetcode"};
+        String chars2 = "welldonehoneyr";
+
+        System.out.println("Example 2 Output: " +
+                obj.countCharacters(words2, chars2));
+    }
+}
